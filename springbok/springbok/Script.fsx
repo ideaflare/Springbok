178ad85d132f03d4c3d4fcc50acbcca710942adc
ideaflare <11017081+ideaflare@users.noreply.github.com>
2018-02-08 22:08:14 +0200

    Add FSharp.Data and parse timestamp

---
+++ springbok/springbok/Script.fsx
@@ -0,0 +1,16 @@
+#load "springbok.fs"
+open System
+open springbok
+
+// Define your library scripting code here
+
+#r "../../packages/FSharp.Data/lib/net45/FSharp.Data.dll"
+open FSharp.Data
+
+let url = "https://api.mybitx.com/api/1/ticker?pair=XBTZAR"
+
+type TickerPair = JsonProvider<"""{"timestamp":1518119215462,"bid":"105499.00","ask":"105500.00","last_trade":"105499.00","rolling_24_hour_volume":"991.43949","pair":"XBTZAR"}""">
+
+let tickerNow = TickerPair.Load(url)
+
+let timeNow = System.DateTimeOffset.FromUnixTimeMilliseconds(tickerNow.Timestamp).UtcDateTime
---
+++ springbok/springbok/springbok.fs
@@ -0,0 +1,6 @@
+namespace springbok
+
+open FSharp.Data
+
+type springbok() = 
+    member this.X = "F#"
